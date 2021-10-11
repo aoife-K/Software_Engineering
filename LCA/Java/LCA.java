3ca7d835b83a7ae1de3793e245bbd98ae172ad8b
aoife-K <khanao@tcd.ie>
2021-10-11 20:04:34 +0100

    Fill in main

--- LCA/Java/LCA.java
+++ LCA/Java/LCA.java
@@ -35,7 +35,6 @@ public class LCA
         int i;
         for (i = 0; i < path1.size() && i < path2.size(); i++) {
              
-        // System.out.println(path1.get(i) + " " + path2.get(i));
             if (!path1.get(i).equals(path2.get(i)))
                 break;
         }
@@ -45,13 +44,11 @@ public class LCA
      
     private boolean findPath(Node root, int n, List<Integer> path)
     {
-        // base case
+        
         if (root == null) {
             return false;
         }
          
-        // Store this node . The node will be removed if
-        // not in path from root to n.
         path.add(root.data);
  
         if (root.data == n) {
@@ -66,13 +63,24 @@ public class LCA
             return true;
         }
  
-        // If not present in subtree rooted with root, remove root from
-        // path[] and return false
         path.remove(path.size()-1);
  
         return false;
     }
     public static void main(String[] args)
     {
+        LCA tree = new LCA();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+ 
+        System.out.println("LCA(4, 5): " + tree.findLCA(4,5));
+        System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
+        System.out.println("LCA(3, 4): " + tree.findLCA(3,4));
+        System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
     }
 }
