ee967a9a03a0e307f589c084512297153138ae60
aoife-K <khanao@tcd.ie>
2021-10-11 18:48:57 +0100

    Include Node class

--- LCA/Java/LCA.java
+++ LCA/Java/LCA.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+ 
+
+class Node {
+    int data;
+    Node left, right;
+ 
+    Node(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+
 public class LCA
 {
 }
