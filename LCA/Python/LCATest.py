16c023b2026ac675fa80ae664b3d978764e60d13
aoife-K <khanao@tcd.ie>
2021-10-14 15:30:56 +0100

    testSimpleTree function

--- LCA/Python/LCATest.py
+++ LCA/Python/LCATest.py
@@ -9,6 +9,17 @@ class TestLowestCommonAncestor:
         assert lca is -1
 
     def testSimpleTree(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+
+        lca = LCA.findLCA(root, 4, 5)
+
+        assert lca.value is 2
   
     def testLongerTree(self):
         
