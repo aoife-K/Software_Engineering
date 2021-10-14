9bff9b204fa847244a84a3ec5db690b0869c58a9
aoife-K <khanao@tcd.ie>
2021-10-14 10:29:12 +0100

    Implement findLCA

--- LCA/Python/LCA.py
+++ LCA/Python/LCA.py
@@ -24,3 +24,15 @@ def findPath( root, path, k):
     return False
 
 def findLCA(root, n1, n2):
+    path1 = []
+    path2 = []
+ 
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
+        return -1
+ 
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        if path1[i] != path2[i]:
+            break
+        i += 1
+    return path1[i-1]
