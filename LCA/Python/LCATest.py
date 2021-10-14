30ac0557667cf0d9b5658956df2c1938611f1a43
aoife-K <khanao@tcd.ie>
2021-10-14 15:28:03 +0100

    testEmpty function

--- LCA/Python/LCATest.py
+++ LCA/Python/LCATest.py
@@ -2,7 +2,12 @@ import LCA
 
 class TestLowestCommonAncestor:
     def testEmpty(self):
-        
+        root = None
+
+        lca = LCA.findLCA(root, 1, 2)
+
+        assert lca is -1
+
     def testSimpleTree(self):
   
     def testLongerTree(self):
