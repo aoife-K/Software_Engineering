dc7cb21dff4eb6cca3c0dcff5ea094a1be5e5f9d
aoife-K <khanao@tcd.ie>
2021-10-14 10:25:51 +0100

    Start Python solution - created functions

---
+++ LCA/Python/LCA.py
@@ -0,0 +1,13 @@
+
+class Node:
+    
+    def __init__(self, key):
+        self.key =  key
+        self.left = None
+        self.right = None
+ 
+
+def findPath( root, path, k):
+ 
+
+def findLCA(root, n1, n2):
