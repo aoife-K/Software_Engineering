435bcccdd0262441e6dacdf87fdd42dc21fb7d9e
aoife-K <khanao@tcd.ie>
2021-10-24 18:42:09 +0100

    Outline test functions

--- LCA/Java/DAGTest.java
+++ LCA/Java/DAGTest.java
@@ -3,5 +3,21 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class DAGTest {
-    
+    @Test
+    public void testEmpty()
+    {
+       DAG graph = new DAG();
+    }
+
+    @Test
+    public void testSimpleDAG()
+    {
+        DAG graph = new DAG();
+    }
+
+    @Test
+    public void testLongerDAG()
+    {
+        DAG graph = new DAG();
+    }
 }
