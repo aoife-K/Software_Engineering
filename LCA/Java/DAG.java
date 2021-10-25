840113bfcb47509cee2799caceee8eb4edd2e21c
aoife-K <khanao@tcd.ie>
2021-10-25 20:48:09 +0100

    Write functions and fix some tests
    Still errors with some tests to be fixed

--- LCA/Java/DAG.java
+++ LCA/Java/DAG.java
@@ -14,17 +14,36 @@ class Node {
 }
 
 public class DAG {
-    public void addEdge(){
-
+    public void addEdge(Node parent, Node child){
+        for (int i = 0; i < parent.succ.size(); i++) {
+			if (!child.succ.contains(parent.succ.get(i)))
+                child.succ.add(parent.succ.get(i));
+		}
     }
 
-    
-
-    public void findDAGLCA(){
-        
+    //acyclic(first, second)
+    public int findDAGLCA(Node root, Node first, Node second){
+        if (first != null && second!= null){
+            if (first.succ != null && second.succ != null) {
+				for (int i = 0; i < second.succ.size(); i++)
+					for (int j = 0; j < first.succ.size(); j++)
+						if (second.succ.get(i) == first.succ.get(j))
+							return second.succ.get(i).data;
+			} else return root.data;
+        } return -1;
     }
 
-    public boolean acyclic(){
+    public boolean acyclic(Node first, Node second){
+        for (int i = 0; i < first.succ.size(); i++){
+            if (first.succ.get(i) == second){
+                return true;
+            }
+        }
         return false;
     }
+
+    public void newNode(Node node) {
+		node.succ = new ArrayList<Node>();
+		node.succ.add(node);
+	}
 }
--- LCA/Java/DAGTest.java
+++ LCA/Java/DAGTest.java
@@ -7,7 +7,7 @@ public class DAGTest {
     public void testEmpty()
     {
        DAG graph = new DAG();
-       assertEquals(null, graph.findDAGLCA(null, null, null));
+       assertEquals(-1, graph.findDAGLCA(null, null, null));
     }
 
     @Test
@@ -22,13 +22,21 @@ public class DAGTest {
         Node n6 = new Node(6);
         Node n7 = new Node(7);
 
-        graph.addEdge(1,3);
-		graph.addEdge(1,5);
-        graph.addEdge(2,3);
+        graph.newNode(n1);
+        graph.newNode(n2);
+        graph.newNode(n3);
+        graph.newNode(n4);
+        graph.newNode(n5);
+        graph.newNode(n6);
+        graph.newNode(n7);
+
+        graph.addEdge(n1,n3);
+		graph.addEdge(n1,n5);
+        graph.addEdge(n2,n3);
         //graph.addEdge(2,5);
-        graph.addEdge(2,4);
-        graph.addEdge(4,7);
-        graph.addEdge(7,6);
+        graph.addEdge(n2,n4);
+        graph.addEdge(n4,n7);
+        graph.addEdge(n7,n6);
 
         assertEquals(2, graph.findDAGLCA(n1, n4, n5));
         assertEquals(2, graph.findDAGLCA(n1, n7, n5));
@@ -51,23 +59,34 @@ public class DAGTest {
         Node n7 = new Node(7);
         Node n8 = new Node(8);
 
-        graph.addEdge(0,1);
-		graph.addEdge(0,2);
-        graph.addEdge(1,4);
-        graph.addEdge(1,6);
-        graph.addEdge(2,4);
-        graph.addEdge(2,6);
-        graph.addEdge(2,3);
-        graph.addEdge(3,6);
-        graph.addEdge(6,5);
-        graph.addEdge(6,7);
-        graph.addEdge(7,8);
+        graph.newNode(n0);
+        graph.newNode(n1);
+        graph.newNode(n2);
+        graph.newNode(n3);
+        graph.newNode(n4);
+        graph.newNode(n5);
+        graph.newNode(n6);
+        graph.newNode(n7);
+        graph.newNode(n8);
+        
+
+        graph.addEdge(n0,n1);
+		graph.addEdge(n0,n2);
+        graph.addEdge(n1,n4);
+        graph.addEdge(n1,n6);
+        graph.addEdge(n2,n4);
+        graph.addEdge(n2,n6);
+        graph.addEdge(n2,n3);
+        graph.addEdge(n3,n6);
+        graph.addEdge(n6,n5);
+        graph.addEdge(n6,n7);
+        graph.addEdge(n7,n8);
 
         assertEquals(1, graph.findDAGLCA(n0, n4, n7));
-        assertEquals(2, graph.findDAGLCA(n0, n4, n7));
+        //assertEquals(2, graph.findDAGLCA(n0, n4, n7));
         assertEquals(6, graph.findDAGLCA(n0, n5, n8));
-        assertEquals(2, graph.findDAGLCA(n0, n6, n3));
-        assertEquals(0, graph.findDAGLCA(n0, n4, n2));
+        assertEquals(1, graph.findDAGLCA(n0, n4, n5));
+        assertEquals(2, graph.findDAGLCA(n0, n4, n2));
         
     }
 
@@ -76,6 +95,7 @@ public class DAGTest {
     {
         DAG graph = new DAG();
         Node n1 = new Node(1);
+        graph.newNode(n1);
         assertEquals(1, graph.findDAGLCA(n1, n1, n1));
     }
 
@@ -88,15 +108,19 @@ public class DAGTest {
         Node n3 = new Node(3);
         Node n4 = new Node(4);
         Node n5 = new Node(5);
-        Node n6 = new Node(6);
-        Node n7 = new Node(7);
+        
+        graph.newNode(n1);
+        graph.newNode(n2);
+        graph.newNode(n3);
+        graph.newNode(n4);
+        graph.newNode(n5);
 
-        graph.addEdge(1,3);
-		graph.addEdge(5,1);
-        graph.addEdge(2,3);
-        graph.addEdge(3,5);
-        graph.addEdge(2,4);
+        graph.addEdge(n1,n3);
+		graph.addEdge(n5,n1);
+        graph.addEdge(n2,n3);
+        graph.addEdge(n3,n5);
+        graph.addEdge(n2,n4);
 
-        assertEquals(null, graph.findDAGLCA(n1, n3, n5));
+        assertEquals(-1, graph.findDAGLCA(n1, n3, n5));
     }
 }
